71a75c95f11f72bec5bce3177eb23ac3d006376f
dennis215 <34072364+dennis215@users.noreply.github.com>
2024-05-24 12:03:55 +0800

    Update cron.py

--- customapp/cron.py
+++ customapp/cron.py
@@ -564,8 +564,10 @@ def doImportCollectionReport():
     # getFileReqUrl = 'http://172.18.96.101:80/internal/SchedulerEOD/RetrieveFiles?ApiKey=lDw6rUrzz5mf7fdNiiAdEdKort5el21TpcmC'
     # returnReqUrl = 'http://172.18.96.101:80/internal/SchedulerEOD/ERPNextFileChecking?ApiKey=vGDkYOrDj5FPhxZrXCKLf5x6lnCIvsSZnsAC'
     # prod
-    getFileReqUrl = 'https://bsportal.indahwater.app/internal/SchedulerEOD/RetrieveFiles?ApiKey=lDw6rUrzz5mf7fdNiiAdEdKort5el21TpcmC'
-    returnReqUrl = 'https://bsportal.indahwater.app/internal/SchedulerEOD/ERPNextFileChecking?ApiKey=vGDkYOrDj5FPhxZrXCKLf5x6lnCIvsSZnsAC'
+    getFileReqUrl = 'http://192.168.200.12:5002/internal/SchedulerEOD/RetrieveFiles?ApiKey=lDw6rUrzz5mf7fdNiiAdEdKort5el21TpcmC'
+    returnReqUrl = 'http://192.168.200.12:5002/internal/SchedulerEOD/ERPNextFileChecking?ApiKey=vGDkYOrDj5FPhxZrXCKLf5x6lnCIvsSZnsAC'
+    # getFileReqUrl = 'https://bsportal.indahwater.app/internal/SchedulerEOD/RetrieveFiles?ApiKey=lDw6rUrzz5mf7fdNiiAdEdKort5el21TpcmC'
+    # returnReqUrl = 'https://bsportal.indahwater.app/internal/SchedulerEOD/ERPNextFileChecking?ApiKey=vGDkYOrDj5FPhxZrXCKLf5x6lnCIvsSZnsAC'
     # Staging
     # getFileReqUrl = 'https://bsstg1.indahwater.app:8443/internal/SchedulerEOD/RetrieveFiles?ApiKey=lDw6rUrzz5mf7fdNiiAdEdKort5el21TpcmC'
     # returnReqUrl = 'https://bsstg1.indahwater.app:8443/internal/SchedulerEOD/ERPNextFileChecking?ApiKey=vGDkYOrDj5FPhxZrXCKLf5x6lnCIvsSZnsAC'
